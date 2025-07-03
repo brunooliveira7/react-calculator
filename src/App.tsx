af527c7d14e780cd9dca12843a3ccda638c71508
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-07-03 15:32:58 -0300

    feat: Implement Button component with styling and add to App
    This commit introduces a reusable Button component with different variants and adds it to the main App component.
    
    - Created a Button component with default and primary variants, styled using Tailwind CSS.
    - Added the Button component to App.tsx with "CE" and "C" labels.
    - Updated index.css to adjust shadow and gradient values.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Text } from "./components/Text";
+import { Button } from "./components/Button";
 
 export function App() {
   return (
    <main className="py-28 px-4">
-    
+    <Button>CE</Button>
+    <Button variant="primary">C</Button>
    </main>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,36 @@
+import { Text } from "./Text";
+
+interface ButtonProps {
+  className?: string;
+  children: React.ReactNode;
+  variant?: keyof typeof buttonVariants;
+}
+
+const buttonVariants = {
+  default: "bg-(--background)",
+  primary: "bg-(--primary)",
+} as const;
+
+export function Button({
+  variant = "default",
+  className,
+  children,
+  ...props
+}: ButtonProps) {
+  return (
+    <Text
+      as="button"
+      variant="heading"
+      className={`
+      flex items-center justify-center rounded-xl p-3 cursor-pointer
+      text-(--text) bg-linear-(--gradient) hover:bg-linear-(--gradient-hover)
+      shadow-(--shadow)
+      ${buttonVariants[variant]}
+      ${className ?? ""}
+    `}
+      {...props}
+    >
+      {children}
+    </Text>
+  );
+}
