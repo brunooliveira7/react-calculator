1e01fd285bcf8edc022bb35f91a3677c1772925e
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-07-03 15:42:27 -0300

    feat: Implement Card component and replace buttons in App
    This commit introduces a Card component for structuring content and replaces the existing buttons in the App component with a Card.
    
    - Created a Card component with styling for background, shadow, and rounded corners using Tailwind CSS.
    - Replaced the Button components in App.tsx with a Card component containing placeholder text.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Text } from "./components/Text";
 import { Button } from "./components/Button";
+import { Card } from "./components/Card";
 
 export function App() {
   return (
    <main className="py-28 px-4">
-    <Button>CE</Button>
-    <Button variant="primary">C</Button>
+    <Card className="p-4">Teste </Card>
    </main>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,15 @@
+type CardProps = React.HTMLAttributes<HTMLDivElement> & {
+  children: React.ReactNode;
+};
+
+export function Card({ children, className, ...props }: CardProps) {
+  return (
+    <div
+      className={`
+    bg-(--background) shadow-(--shadow) rounded-2xl ${className ?? ""}`}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
